e615c87f86cc4c77d8d617ec53b682be41afd6a7
Park Sohee <pssh5124@gmail.com>
2023-01-05 16:14:13 +0900

    35 end

--- components/Car/Card.vue
+++ components/Car/Card.vue
@@ -1,12 +1,17 @@
 <script setup>
+import heartFilled from "@/assets/heartFilled.png";
+import heartOutline from "@/assets/heartOutline.png";
 const props = defineProps({
   car: Object,
+  favored: Boolean,
 });
+const emit = defineEmits(["favor"]);
 </script>
 <template>
-  <div class="shadow border w-full overflow-hidden mb-5 cursor-pointer h-[200px]" @click="navigateTo(`/car/${car.name}-${car.id}`)">
-    <div class="flex h-full">
-      <img :src="car.url" alt="" class="w-[300px] h-full" />
+  <div class="shadow border w-full overflow-hidden mb-5 cursor-pointer h-[200px] relative">
+    <img :src="favored ? heartFilled : heartOutline" alt="" class="absolute w-7 right-5 top-2 z-20" @click="emit('favor', car.id)" />
+    <div class="flex h-full" @click="navigateTo(`/car/${car.name}-${car.id}`)">
+      <NuxtImg :src="car.url" alt="" class="w-[300px] h-full" />
       <div class="p-4 flex flex-col">
         <div class="">
           <h1 class="text-2xl text-blue-700">{{ car.name }}</h1>
--- components/Car/SideBar.vue
+++ components/Car/SideBar.vue
@@ -1,12 +1,36 @@
+<script setup>
+const modal = ref({
+  location: false,
+  make: false,
+  price: false,
+});
+const route = useRoute();
+const city = ref("");
+const updateModal = (key) => {
+  modal.value[key] = !modal.value[key];
+};
+const onChangeLocation = () => {
+  if (!city.value) return;
+  if (!isNaN(parseInt(city.value))) {
+    throw createError({
+      statusCode: 404,
+      message: "Invalid city format",
+    });
+  }
+  updateModal("location");
+  navigateTo(`/city/${city.value}/car`);
+  city.value = "";
+};
+</script>
 <template>
   <div class="shadow border w-64 mr-10 z-30 h-[190px]">
     <div class="p-5 flex justify-between relative cursor-pointer border-b">
       <h3>Location</h3>
-      <h3 class="text-blue-400 capitalize">Toronto</h3>
-      <!-- <div class="absolute border shadow left-56 p-5 top-1 -m-1 bg-white">
-              <input type="text" class="border p-1 rounded" />
-              <button class="bg-blue-400 w-full mt-2 rounded text-white p-1">Apply</button>
-            </div> -->
+      <h3 class="text-blue-400 capitalize" @click="updateModal('location')">{{ route.params.city }}</h3>
+      <div class="absolute border shadow left-56 p-5 top-1 -m-1 bg-white" v-if="modal.location">
+        <input type="text" class="border p-1 rounded" v-model="city" />
+        <button class="bg-blue-400 w-full mt-2 rounded text-white p-1" @click="onChangeLocation">Apply</button>
+      </div>
     </div>
     <div class="p-5 flex justify-between relative cursor-pointer border-b">
       <h3>Make</h3>
